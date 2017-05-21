f519bafd7ad84fe1aba8e73a4e0d7ca4410a6373
Graham Anderson <grahama@me.com>
2017-05-21 17:03:35 -0500

    Adding Nav CSS

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,44 +2,67 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 
-const Nav = () => {
-    return (
-      <div>
-        <h2>Nav Component</h2>
-        <NavLink exact to="/"
-          activeClassName="active"
-          activeStyle={{color:'red'}}>
-          Get Weather
-        </NavLink>
+class Nav extends React.Component {
 
-        <NavLink to="/about"
-          activeClassName="active"
-          activeStyle={{color: 'red'}}>
-          About
-        </NavLink>
-
-        <NavLink to="/examples"
-          activeClassName="active"
-          activeStyle={{color: 'red'}}>
-          Examples
-        </NavLink>
-      </div>
-    )
+  constructor(props) {
+    super(props);
+    this.onSearch = this.onSearch.bind(this)
   }
 
-export default Nav;
+  onSearch(e){
+    e.preventDefault();
+    alert('Not yet wired up!')
+  }
+
+  render() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+
+            <li>
+              <NavLink exact to="/"
+                activeClassName="active"
+                activeStyle={{color:'red'}}>
+                Get Weather
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/about"
+                activeClassName="active"
+                activeStyle={{color: 'red'}}>
+                About
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/examples"
+                activeClassName="active"
+                activeStyle={{color: 'red'}}>
+                Examples
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className= "top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+      )
+    }
+  }
 
 
-
-// class About extends React.Component {
-//
-//   constructor (props) {
-//     super(props);
-//   }
-//
-//   render() {
-//     return(
-//       <h3>About Component</h3>
-//     )
-//   }
-// }
+  export default Nav;
