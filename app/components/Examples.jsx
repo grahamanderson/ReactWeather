3da706bf5da7a7c71b1c8f9deceb1b4bd1dfc566
Graham Anderson <grahama@me.com>
2017-05-21 12:27:26 -0500

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Examples = () => {
   // var fish = getFish(props.species);
   return (
-    <h1>Here are Examples!</h1>
+<div>
+  <h1>Here are Examples!</h1>
+  <p>Welcome to the examples page</p>
+</div>
   )
 };
 
